6ce4e437ccbd643054e7f34850b6245127f996b4
rockuo <richardbures@email.cz>
2019-03-26 17:57:27 +0100

    Registrace s RFID default IP

--- src/Form/UserType.php
+++ src/Form/UserType.php
@@ -4,9 +4,11 @@ namespace App\Form;
 
 use App\Service\ApiMiddleware;
 use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\Extension\Core\Type\ButtonType;
 use Symfony\Component\Form\Extension\Core\Type\CheckboxType;
 use Symfony\Component\Form\Extension\Core\Type\ChoiceType;
 use Symfony\Component\Form\Extension\Core\Type\EmailType;
+use Symfony\Component\Form\Extension\Core\Type\HiddenType;
 use Symfony\Component\Form\Extension\Core\Type\NumberType;
 use Symfony\Component\Form\Extension\Core\Type\SubmitType;
 use Symfony\Component\Form\Extension\Core\Type\TextType;
@@ -22,7 +24,9 @@ class UserType extends AbstractType
             ->add('gamification_consent', CheckboxType::class, ['required' => false])
             ->add('name')
             ->add('nickname', TextType::class,['required' => false])
-            ->add('role', ChoiceType::class, ['choices' => array_flip(ApiMiddleware::USER_ROLES)]);
+//            ->add('role', ChoiceType::class, ['choices' => array_flip(ApiMiddleware::USER_ROLES)])
+//            ->add('load', ButtonType::class, ['label' => 'Přečít ID'])
+            ->add('rfid', HiddenType::class,['required' => false]);
 
         if($options['me'])
         {

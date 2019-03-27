aaee24680542c9c79f0b919392a32fb1b01d8981
rockuo <richardbures@email.cz>
2019-03-27 18:57:05 +0100

    zmena role -fix

--- src/Form/UserType.php
+++ src/Form/UserType.php
@@ -24,9 +24,9 @@ class UserType extends AbstractType
             ->add('gamification_consent', CheckboxType::class, ['required' => false])
             ->add('name')
             ->add('nickname', TextType::class,['required' => false])
-//            ->add('role', ChoiceType::class, ['choices' => array_flip(ApiMiddleware::USER_ROLES)])
+            ->add('role', ChoiceType::class, ['choices' => array_flip(ApiMiddleware::USER_ROLES)]);
 //            ->add('load', ButtonType::class, ['label' => 'Přečít ID'])
-            ->add('rfid', HiddenType::class,['required' => false]);
+//            ->add('rfid', HiddenType::class,['required' => false]);
 
         if($options['me'])
         {
